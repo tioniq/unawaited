4f42f84a553b8c81b22e9025fe97e8a13b00662a
Timoniann <timoniann@gmail.com>
2024-10-08 18:29:46 +0300

    build(release): bump version to 1.0.1 build: upload to jsr pm

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,17 @@
 type PromiseOrNothing = Promise<any> | PromiseLike<any> | undefined | null | void;
 /**
+ * A source key that can be used to identify the source of the error
+ */
+type Source = Symbol | string | number | object | null | undefined;
+/**
+ * A callback that takes an error as an argument
+ */
+type ErrorCallback = (e: any) => void;
+/**
+ * A callback that takes an error as an argument or a source key
+ */
+type ErrorCallbackOrSource = ErrorCallback | Source;
+/**
  * A function that takes a promise or function that returns a promise and handles it to prevent unhandled promise
  * rejections. It will never throw an error. If the promise or function throws an error, it will be caught and passed
  * to the `unawaited.exceptionHandler` function. If the promise or function is not a promise or function, it will be
@@ -7,12 +19,13 @@ type PromiseOrNothing = Promise<any> | PromiseLike<any> | undefined | null | voi
  * the source of the promise or function if needed.
  *
  * @param promiseOrFunction The promise or function that returns a promise to handle
- * @param source The source of the promise or function
+ * @param errorCallbackOrSource The source of the promise or function
  */
-declare function unawaited(promiseOrFunction: (() => PromiseOrNothing) | PromiseOrNothing, source?: any): void;
+declare function unawaited(promiseOrFunction: (() => PromiseOrNothing) | PromiseOrNothing, errorCallbackOrSource?: ErrorCallbackOrSource): void;
 declare namespace unawaited {
-    var exceptionHandler: (e: any, source: any) => void;
-    var unknownValueHandler: (value: any, source: any) => void;
+    var exceptionHandler: (e: any, source: ErrorCallbackOrSource) => void;
+    var unknownValueHandler: (value: any, source: ErrorCallbackOrSource) => void;
+    var alwaysPassExceptionToHandler: boolean;
 }
 
-export { unawaited };
+export { type ErrorCallback, type ErrorCallbackOrSource, type Source, unawaited };
--- dist/index.js
+++ dist/index.js
@@ -1,56 +1,67 @@
 // src/index.ts
-function unawaited(promiseOrFunction, source) {
+function unawaited(promiseOrFunction, errorCallbackOrSource) {
   if (!promiseOrFunction) {
     return;
   }
   if (promiseOrFunction instanceof Promise) {
-    handlePromise(promiseOrFunction, source);
+    handlePromise(promiseOrFunction, errorCallbackOrSource);
     return;
   }
   if (typeof promiseOrFunction !== "function") {
     if (typeof promiseOrFunction === "object") {
       if (typeof promiseOrFunction.then === "function") {
-        handlePromiseLike(promiseOrFunction, source);
+        handlePromiseLike(promiseOrFunction, errorCallbackOrSource);
         return;
       }
     }
-    unawaited.unknownValueHandler(promiseOrFunction, source);
+    unawaited.unknownValueHandler(promiseOrFunction, errorCallbackOrSource);
     return;
   }
   let result;
   try {
     result = promiseOrFunction();
   } catch (e) {
-    unawaited.exceptionHandler(e, source);
+    handleException(e, errorCallbackOrSource);
   }
   if (result instanceof Promise) {
-    handlePromise(result, source);
+    handlePromise(result, errorCallbackOrSource);
     return;
   }
   if (typeof result === "object") {
     if (typeof result.then === "function") {
-      handlePromiseLike(result, source);
+      handlePromiseLike(result, errorCallbackOrSource);
     }
   }
 }
-function handlePromise(promise, source) {
+function handlePromise(promise, errorCallbackOrSource) {
   try {
-    promise.catch((e) => unawaited.exceptionHandler(e, source));
+    promise.catch((e) => handleException(e, errorCallbackOrSource));
   } catch (e) {
-    unawaited.exceptionHandler(e, source);
+    handleException(e, errorCallbackOrSource);
   }
 }
-function handlePromiseLike(promiseLike, source) {
+function handlePromiseLike(promiseLike, errorCallbackOrSource) {
   try {
-    Promise.resolve(promiseLike).catch((e) => unawaited.exceptionHandler(e, source));
+    Promise.resolve(promiseLike).catch((e) => handleException(e, errorCallbackOrSource));
   } catch (e) {
-    unawaited.exceptionHandler(e, source);
+    handleException(e, errorCallbackOrSource);
+  }
+}
+function handleException(e, errorCallbackOrSource) {
+  if (typeof errorCallbackOrSource !== "function") {
+    unawaited.exceptionHandler(e, errorCallbackOrSource);
+    return;
+  }
+  errorCallbackOrSource(e);
+  if (unawaited.alwaysPassExceptionToHandler) {
+    unawaited.exceptionHandler(e, errorCallbackOrSource);
   }
 }
 unawaited.exceptionHandler = function(e, source) {
 };
 unawaited.unknownValueHandler = function(value, source) {
 };
+unawaited.alwaysPassExceptionToHandler = true;
 export {
   unawaited
 };
